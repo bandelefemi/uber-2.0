6674ca8647c1cbe69dc655d4a6edea1f0d4b0985
Femi Bandele <105954155+bandelefemi@users.noreply.github.com>
2022-11-03 19:24:04 -0400

    home screen build

--- GlobalStyles.js
+++ GlobalStyles.js
@@ -4,7 +4,6 @@ import { StyleSheet, Platform } from 'react-native';
 export default StyleSheet.create({
     droidSafeArea: {
         flex: 1,
-        backgroundColor: 'whitesmoke',
         paddingTop: Platform.OS === 'android' ? 25 : 0
     },
 });
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,23 @@
 import { StyleSheet, Text, View, Platform, 
-    StatusBar,SafeAreaView } from 'react-native'
+    StatusBar,SafeAreaView, Image } from 'react-native'
 import React from 'react'
 import GlobalStyles from '../GlobalStyles'
+import tw from 'tailwind-react-native-classnames'
+import NavOptions from '../components/NavOptions'
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView style={GlobalStyles.droidSafeArea} >
-      <View>
-        <Text style={styles.text}>I am the light!!!</Text>
+    <SafeAreaView style={[tw`bg-white`, GlobalStyles.droidSafeArea]} >
+      <View style={tw` p-5`}>
+        <Image
+            style={{
+                width: 100, height: 100, resizeMode: 'contain'
+            }} 
+            source={{
+                uri: 'https://links.papareact.com/gzs'
+            }} />
+
+        <NavOptions />
       </View>
     </SafeAreaView>
   )
